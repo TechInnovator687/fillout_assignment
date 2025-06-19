3861c613d3897dd469cd74e9d9262c34bfaa0c77
Tech Innovator <chetanrathore687@gmail.com>
2025-06-19 17:32:00 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { PagesManager } from "./components/organisms/PagesManager";
-import { PageProvider } from "./context/PageContext";
+
 
 const App: React.FC = () => {
   return (
     <div>
-      <PageProvider>
         <PagesManager />;
-      </PageProvider>
     </div>
   );
 };
